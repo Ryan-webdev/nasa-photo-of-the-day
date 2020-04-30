c6b2b8535a85ac71888e8b27e1643a4f4e5e4938
Ryan Schueck <ryschueck@gmail.com>
2020-04-29 21:43:04 -0400

    finished day 2 of project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
-import Background from './components/background'
+import Background from './components/background';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
-      <p>
+      
         <Background />
-      </p>
+      
     </div>
   );
 }
--- src/components/background.js
+++ src/components/background.js
@@ -2,12 +2,17 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Tiles from './tiles'
 
-console.log(axios.get('https://api.nasa.gov/planetary/apod?api_key=2IpkSRXQGSAuFz3CSy8WOj65EQKqUHIwJcYaMah1'))
-
+// fuction to setup the data from the nasa api to link to our main js file.
 function Background(){
+
+    //setting the initial state
     const [data, setData] = useState([]);
 
+
+    //initial render of the nasa api.
+    //useEffect should only send a get request once unless the user reloads the page.
     useEffect(() => {
+        //getting the nasa API through axios.
         axios
         .get(`https://api.nasa.gov/planetary/apod?api_key=2IpkSRXQGSAuFz3CSy8WOj65EQKqUHIwJcYaMah1`)
         .then (res => {
@@ -20,7 +25,8 @@ function Background(){
 
 
 
-
+    //returns jsx from my tiles js file.
+    //created a Tiles tag return the tile value and passing data as the initial state.
     return(
         <div>
         <Tiles tile={data} />
